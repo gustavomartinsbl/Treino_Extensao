a78bfd982a1f2303cdedfc85301eee68d0058929
gustavomartinsbl <gu_martins@id.uff.br>
2026-09-14 10:42:22 -0300

    script - tarefa 1 a 2

--- script_BDAM.R
+++ script_BDAM.R
@@ -215,6 +215,25 @@ head(dados_bd2)
 # Atribuir legendas para a variável TIPO_VEICULO, sendo 1: Carro e 2: Moto
 # Criar uma nova variável em dados_bd2 F_IDADE categorizando as idades em: 22 a 34, 35 a 45
 
+dados_bd2$SEXO_PROPRIETARIO[dados_bd2$SEXO_PROPRIETARIO == 1] <- "Masculino"
+dados_bd2$SEXO_PROPRIETARIO[dados_bd2$SEXO_PROPRIETARIO == 2] <- "Feminino"
+
+dados_bd2$TIPO_VEICULO <- factor(
+  dados_bd2$TIPO_VEICULO,
+  levels = c(1, 2),
+  labels = c("Carro", "Moto")
+)
+
+dados_bd2$F_IDADE <- cut(
+  dados_bd2$IDADE_PROPRIETARIO,
+  breaks = c(21, 34, 45),
+  labels = c("22 a 34", "35 a 45")
+)
+
+table(dados_bd2$SEXO_PROPRIETARIO, useNA = "ifany")
+table(dados_bd2$TIPO_VEICULO, useNA = "ifany")
+table(dados_bd2$F_IDADE, useNA = "ifany")
+
 # Ao terminar a Tarefa 2 commit com a mensagem " script - tarefa 1 a 2" e envie para o repositório Treino_Extensao
 
 
